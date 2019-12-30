3e236ebac0dc800a11cc20e45880b451748a5bc4
Sinan Kozak <sinan.kozak@scout24.com>
2019-12-30 17:35:50 +0300

    buildSrc for library module

---
+++ BasicSample/buildSrc/src/main/kotlin/de/is24/plugins/LibraryCommonPlugin.kt
@@ -0,0 +1,10 @@
+package de.is24.plugins
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LibraryCommonPlugin : Plugin<Project> {
+
+  override fun apply(target: Project) {
+  }
+}
